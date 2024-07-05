0ad70f54a08665878da6a96cfa8dc4420700b8eb
liangchj <752304023@qq.com>
2024-07-06 00:56:17 +0800

    调整

--- lib/render_object_danmaku.dart
+++ lib/render_object_danmaku.dart
@@ -4,3 +4,5 @@ export 'danmaku_view.dart';
 export 'danmaku_controller.dart';
 export './models/danmaku_option.dart';
 export './models/danmaku_item.dart';
+export './parse/base_danmaku_parser.dart';
+export './parse/bili_danmaku_parser.dart';
